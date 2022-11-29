d805185f4164246d374c12f445a19563d6dcd0c9
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-11-29 21:05:22 +0100

    new: bootstrap

---
+++ frontend/src/components/Map.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { Map } from "leaflet";
+
+  function setMap(mapElement: HTMLElement) {
+    (async () => {
+      const L = await import("leaflet");
+      const { addHouseToMap, addGasStationsToMap } = await import(
+        "../utils/map-utils"
+      );
+
+      var map: Map = L.map(mapElement).setView(
+        [coordinates.latitude, coordinates.longitude],
+        13
+      );
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      addHouseToMap(L, coordinates, map);
+      addGasStationsToMap(L, gasStations, map);
+    })();
+  }
+
+  /**
+   * param: coordinates: Coordinates
+   *
+   * Coordinates of the house
+   */
+  export let coordinates: Coordinates;
+  /**
+   * param: gasStations: EESSPrecio[]
+   *
+   * Array of gas stations to be displayed as markers
+   */
+  export let gasStations: EESSPrecio[];
+</script>
+
+<figure class="map" use:setMap />
