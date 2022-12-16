70fed73a99597d88ab2b72965404b96bb3198ae6
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-12-16 19:55:25 +0100

    removed example

--- frontend/src/pages/example/cloudinaryHandler.js
+++
@@ -1,7 +0,0 @@
-export async function post({ request }) {
-  const formData = await request.formData();
-  console.log(formData);
-  return {
-    body: JSON.stringify({ redirect: formData.get("url") }),
-  };
-}
